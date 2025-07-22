b46b0dfe18a7f3e2f76c8510a8b92227cf5daeec
André Guimarães Barros <102279789+Dederso@users.noreply.github.com>
2025-07-21 22:54:59 -0300

    upload map

---
+++ mapa poke/Tileset Spring8.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tileset Spring" tilewidth="8" tileheight="8" tilecount="960" columns="24">
+ <image source="Farm RPG FREE 16x16 - Tiny Asset Pack/Tileset/Tileset Spring.png" width="192" height="320"/>
+</tileset>
